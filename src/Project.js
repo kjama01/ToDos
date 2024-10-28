401765978e86c086893882cff19bba2fc121f741
Donquellso <kamil080801@gmail.com>
2024-10-28 23:06:58 +0100

    Finish

--- src/Project.js
+++ src/Project.js
@@ -20,18 +20,30 @@ getTasks(){
 addTask(task){
     if(this.tasks.find(element => element.title === task)) return 
     this.tasks.push(new tasks(task, "No date", 0));
+    console.log(this);
 }
 deleteTask(task){
     this.tasks = this.tasks.filter((element) => element.title !== task);
 }
 findTask(task){
-    return (this.tasks.find(element => element.title === task));
+    return this.tasks.find(element => 
+        element.title.trim() === task.trim()
+    );
 }
 todayTasks(){
     const now = new Date();
     const formattedDate = format(now, 'yyyy-MM-dd');
     let today = this.tasks.filter(task=>task.dueDate===formattedDate);
     return today;
+}
+thisWeekTasks(){
+    const now = new Date();
+    const oneWeekInMilliseconds = 7 * 24 * 60 * 60 * 1000; 
+    let thisWeek = this.tasks.filter(task=>{
+        let taskDueDate = new Date(task.dueDate);
+        let timeDifference = taskDueDate.getTime()-now.getTime()
+        return timeDifference<=oneWeekInMilliseconds && timeDifference>0 });
+    return thisWeek;
 }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,39 +1,69 @@
 import project from './Project.js';
 import { format } from 'date-fns';
-
+import Task from './Task.js';
 const now = new Date();
 const formattedDate = format(now, 'dd/MM/yyyy');
 
-// console.log(`Formatted date: ${formattedDate}`);
 
 let tab = [];
+let Inbox = new project("Inbox");
+let Today = new project("Today");
+let thisWeek = new project("thisWeek");
+let Default = new project("Default");
 
+tab.push(Inbox);
+tab.push(Today);
+tab.push(thisWeek);
+tab.push(Default);
 
 class UI{
 
     static loadElements(){
+        UI.getLocalStorage();
         UI.loadProjects();
         UI.initProjectsAddButtons();
         UI.initBasicButtons();
-        UI.loadInbox();
+        UI.getProject(Inbox);
     }
-
+    static setLocalStorage(){
+        console.log(tab);
+        tab[0]=Inbox;
+        console.log(tab);
+        localStorage.setItem("tab",JSON.stringify(tab));
+    }
+    static getLocalStorage() {
+        let storage = localStorage.getItem("tab");
+        if (storage !== null) {
+            let tempTab = JSON.parse(storage);
+            tab = tempTab.map(projectData => { 
+                let newProject = new project(projectData.name); 
+                projectData.tasks.forEach(task => {
+                    const newTask = new Task(task.title, task.dueDate, task.priority);
+                    newProject.tasks.push(newTask); 
+                });
+                return newProject; 
+            });
+        }
+    }
     static loadProjects(){
         let projectslist = document.getElementById("projects-list");
         projectslist.innerHTML="";
         tab.forEach((element)=>{
-            if(element.name=="Inbox") {return};
+            if (["Inbox", "Today", "thisWeek"].includes(element.name)) {return;}
         let projectname = document.createElement("button");
         projectname.innerHTML=`<i class="fa-brands fa-elementor"></i> ${element.name} `
         projectname.classList.add("project-name-button");
+        projectname.addEventListener('click',()=>{UI.getProject(element)});
         projectslist.appendChild(projectname);
     });
     }
     static initBasicButtons(){
         let inbox = document.querySelector(".inbox-button");
         let today = document.querySelector(".today-button");
-        inbox.addEventListener('click',UI.loadInbox);
-        today.addEventListener('click',UI.loadToday);
+        let thisweek = document.querySelector(".thisweek-button");
+        inbox.addEventListener('click', () => UI.getProject(Inbox)); 
+        today.addEventListener('click',() => UI.getProject(Today));
+        thisweek.addEventListener('click',() => UI.getProject(thisWeek));
     }
     static initProjectsAddButtons(){
         let addProjectButton = document.querySelector(".add-project");
@@ -51,6 +81,7 @@ class UI{
                 alert("Too short name");
             }else{
             tab.push(new project(popupinput.value));
+            UI.setLocalStorage();
             UI.loadProjects();
             UI.closePopups();
             }
@@ -60,14 +91,14 @@ class UI{
     static closePopups(){
         document.getElementById("addprojectpopup").classList.add("popup-close");
         document.querySelector(".add-project").classList.remove("popup-close");
-        document.getElementById("addtaskpopup").classList.add("popup-close");
+        document.getElementById("addtaskpopup")?.classList.add("popup-close");
         document.querySelectorAll(".inputTask").forEach((input)=>{
             input.classList.add("closed");
         });
         document.querySelectorAll(".elementtitle").forEach((element)=>{
             element.classList.remove("closed");
         });
-        document.querySelector(".add-task").classList.remove("popup-close");
+        document.querySelector(".add-task")?.classList.remove("popup-close");
         document.querySelectorAll(".dateinput").forEach((date)=>{
             date.classList.add("closed");
         });
@@ -75,43 +106,7 @@ class UI{
             datetext.classList.remove("closed");
         });
     }
-
-    static loadInbox(){
-        rightpanelheader.innerHTML="";
-        rightpanelheader.innerHTML=`
-        <h3>Inbox</h3>	
-        <div id="inboxtasklist"></div>
-        <button class="add-task">
-                <i class="fa-solid fa-plus"></i>
-                Add Task
-        </button>
-        <div id="addtaskpopup" class="popup-close">
-        <input type="text" id="popuptaskinput"><br>
-        <button type="button" id="popuptasksubmitbutton">Add</button>
-        <button type="button" id="popuptaskclosebutton">Close</button>
-    </div>`
-    let inboxtasklist = document.getElementById("inboxtasklist");
-    tab[0].tasks.forEach((element, index)=>{
-        let taskdiv = document.createElement("div");
-        const classPriority = !element.priority ? "fa-regular fa-star priority" : "fa-solid fa-star priority";
-        const className = !element.priority ? "taskdiv" : "taskdivpriority";
-        taskdiv.classList.add(className);
-        taskdiv.innerHTML=`
-        <div id="left">
-        <span class="elementtitle" dataindex="${index}">${element.title}</span>
-        </div>
-        <div id="right">
-        <span class="datetext" data-index="${index}">${element.dueDate}</span>
-        <span><i class="${classPriority}" data-index=${index}></i></span>
-        <span><i class="fa-solid fa-xmark deletetask" data-index=${index}></i></span>
-        </div>`
-        //innerHTML niezalecane ale w tym przypadku git
-        inboxtasklist.appendChild(taskdiv);
-    });
-    UI.initTaskAddButtons();
-    UI.initTaskButtonsListeners()
-    }
-    static initTaskAddButtons(){
+    static initTaskAddButtons(projectName){
         let addtaskbutton = document.querySelector(".add-task");
         let addtaskpopup = document.getElementById("addtaskpopup");
         let popuptasksubmitbutton = document.getElementById("popuptasksubmitbutton");
@@ -124,12 +119,17 @@ class UI{
         });
         popuptasksubmitbutton.addEventListener('click',()=>{
             if(popuptaskinput.value<1){alert("Cant be empty"); return }
-            Inbox.addTask(popuptaskinput.value);
-            UI.loadInbox();
+
+            projectName.addTask(popuptaskinput.value);
+            console.log(popuptaskinput.value);
+            console.log(projectName);
+            console.log(tab);
+            UI.setLocalStorage();
+            UI.getProject(projectName);
         });
         popuptaskclosebutton.addEventListener('click',UI.closePopups);
     } 
-    static initTaskButtonsListeners(){
+    static initTaskButtonsListeners(projectName){
         let tasktitle = document.querySelectorAll(".elementtitle");
         let deletetask = document.querySelectorAll(".deletetask");
         let dateinput = document.querySelectorAll(".datetext");
@@ -137,19 +137,21 @@ class UI{
 
         tasktitle.forEach((title)=>{
             title.addEventListener('click',function(){
-                UI.renameTask(title)});
+                UI.renameTask(projectName, title)});
         });
         deletetask.forEach((element)=>{
-            element.addEventListener('click',UI.deleteTask);
-        });
+            element.addEventListener('click',(e)=>{
+                console.log("Task delete button clicked:", e.target.dataset.index);
+                UI.deleteTask(projectName, e)});
+            });
         dateinput.forEach((date)=>{
             date.addEventListener('click',function(){
-                UI.changeDate(date);
+                UI.changeDate(projectName, date);
             });
         });
         priorities.forEach((priority)=>{
             priority.addEventListener('click',function(){
-                UI.changePriority(priority);
+                UI.changePriority(projectName, priority);
             })
         })
         /////DO EDYCJI
@@ -160,12 +162,12 @@ class UI{
         })
         ///////////
     }
-    static changePriority(elementpr){
+    static changePriority(projectName, elementpr){
         let id = elementpr.dataset.index;
-        tab[0].tasks[id].priority=!tab[0].tasks[id].priority;
-        UI.loadInbox();
+        projectName.tasks[id].priority=!projectName.tasks[id].priority;
+        UI.getProject(projectName);
     }
-    static changeDate(date){
+    static changeDate(projectName, date){
         UI.closePopups();
         let dateinput = document.createElement("input");
         dateinput.type="date";
@@ -175,14 +177,12 @@ class UI{
         date.classList.add("closed");
         dateinput.addEventListener('keydown',(event)=>{
             if(event.key==="Enter" && dateinput.value!=""){
-                tab[0].tasks[date.dataset.index].setTaskdueDate(dateinput.value);
-                UI.loadInbox();
-                console.log(tab[0].tasks[date.dataset.index])
-                Inbox.todayTasks();
+                projectName.tasks[date.dataset.index].setTaskdueDate(dateinput.value);
+                UI.getProject(projectName);
             }
         });      
     }
-    static renameTask(title){
+    static renameTask(projectName, title){
         UI.closePopups();
         let inputTask = document.createElement("input");
         inputTask.type="text";
@@ -191,37 +191,100 @@ class UI{
         const parentNode = title.parentNode;
         const newTitle = title.textContent;
         title.classList.add("closed");
-        parentNode.appendChild(inputTask);
+        parentNode.insertBefore(inputTask, parentNode.firstChild);
         inputTask.focus();
         inputTask.addEventListener('click', (event) => {
             event.stopPropagation();
           });
         inputTask.addEventListener('keydown',(event)=>{
             if(event.key==="Enter"){
-                const tempTask = Inbox.findTask(newTitle);
+                const tempTask = projectName.findTask(newTitle);
                tempTask.setTaskTitle(inputTask.value);
-               UI.loadInbox();
+               UI.getProject(projectName);
         }
         
         })
     }
-    static deleteTask(e){        
-        Inbox.deleteTask(Inbox.tasks[e.target.dataset.index].title)
-        UI.loadInbox();
+    static deleteTask(projectName, e){
+        if(projectName===Today || projectName===thisWeek){ //szukanie we wszystkich projektach gdzie znajduje sie ten task
+            const foundTask = tab.find(element => {
+                if (element.name === "Today" || element.name === "thisWeek") {
+                    return false;
+                }
+                return element.tasks.find(task => {
+                    return projectName.tasks[e.target.dataset.index].title === task.title;
+                });
+            });
+            foundTask.deleteTask(projectName.tasks[e.target.dataset.index].title)
+        }
+        projectName.deleteTask(projectName.tasks[e.target.dataset.index].title)
+        UI.getProject(projectName);
     }
-    static loadToday(){
-        rightpanelheader.innerHTML="";
-        rightpanelheader.innerHTML=`Today<div id="todaytasklist"></div>`;
-        let inboxtasklist = document.getElementById("todaytasklist");
-        let todayTab = Inbox.todayTasks();
-    todayTab.forEach((element, index)=>{
+static getProject(projectName){
+    console.log(tab);
+    UI.setLocalStorage();
+    if(projectName===Today){
+        Today.tasks="";
+        tab.forEach((element)=>{
+            if(element.name==="Today" || element.name==="thisWeek"){
+                return;
+            };
+        let todayTab = element.todayTasks();
+        todayTab.forEach(task => {
+            task.projectName = element.name;
+        });
+        Today.tasks = [...Today.tasks, ...todayTab];
+        
+        console.log(Today.tasks);
+        });
+    }
+    if(projectName===thisWeek){
+        thisWeek.tasks="";
+        tab.forEach((element)=>{
+            if(element.name==="Today" || element.name==="thisWeek"){
+                return;
+            };
+            console.log(element);
+            let thisWeekTab = element.thisWeekTasks();
+            thisWeekTab.forEach(task => {
+                task.projectName = element.name;
+            });
+            thisWeek.tasks=[...thisWeek.tasks, ...thisWeekTab];
+        });
+    }
+    UI.loadTasks(projectName);
+}
+
+ static loadTasks(projectName){
+    rightpanelheader.innerHTML=`<h3>${projectName.name}</h3>
+            <div id="tasklist"></div>`;
+    if(projectName.name!=="Today" && projectName.name!=='thisWeek'){
+        rightpanelheader.innerHTML+=`
+        <button class="add-task">
+                <i class="fa-solid fa-plus"></i>
+                Add Task
+        </button>
+        <div id="addtaskpopup" class="popup-close">
+        <input type="text" id="popuptaskinput"><br>
+        <button type="button" id="popuptasksubmitbutton">Add</button>
+        <button type="button" id="popuptaskclosebutton">Close</button>
+    </div>`
+    UI.initTaskAddButtons(projectName);
+    }
+    let tasklist = document.getElementById("tasklist");
+        projectName.tasks.forEach((element, index)=>{
+            console.log(element);
         let taskdiv = document.createElement("div");
         const classPriority = !element.priority ? "fa-regular fa-star priority" : "fa-solid fa-star priority";
         const className = !element.priority ? "taskdiv" : "taskdivpriority";
+        let projectNameInfo = (projectName.name==="Today" || projectName.name==="thisWeek") ?  `(${element.projectName})` : "";
         taskdiv.classList.add(className);
         taskdiv.innerHTML=`
         <div id="left">
         <span class="elementtitle" dataindex="${index}">${element.title}</span>
+        </div>
+        <div id="mid">
+        <span class="projectNameInfo">${projectNameInfo}</span>
         </div>
         <div id="right">
         <span class="datetext" data-index="${index}">${element.dueDate}</span>
@@ -229,20 +292,15 @@ class UI{
         <span><i class="fa-solid fa-xmark deletetask" data-index=${index}></i></span>
         </div>`
         //innerHTML niezalecane ale w tym przypadku git
-        inboxtasklist.appendChild(taskdiv);
+        tasklist.appendChild(taskdiv);
     });
-    UI.initTaskButtonsListeners()
-
+    UI.initTaskButtonsListeners(projectName)
  }
 }
 
 
 
-let Inbox = new project("Inbox");
-let Default = new project("Default");
-tab.push(Inbox);
-tab.push(Default);
 
-console.log(tab);
+
 
 document.addEventListener('DOMContentLoaded', UI.loadElements)
